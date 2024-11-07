6f330220f02cfce12b4702ac5941d8f8d6c56972
Toffee3 <119606870+TaufiqMohammad@users.noreply.github.com>
2024-11-07 16:58:57 +0530

    Update tokenizer.py
    Signed-off-by: Toffee3 <119606870+TaufiqMohammad@users.noreply.github.com>

--- tokenizer.py
+++ tokenizer.py
@@ -1,31 +1,39 @@
-# tokenizer.py
 import re
+
 
 def tokenize(code):
     token_specification = [
-        ('NUMBER', r'\d+'),
-        ('SET', r'Set'),
-        ('TO', r'to'),
-        ('PRINT', r'Print'),
-        ('IF', r'If'),
-        ('THEN', r'Then'),
-        ('ELSE', r'Else'),
-        ('ENDIF', r'End If'),
-        ('GT', r'>'),
-        ('LT', r'<'),
-        ('EQ', r'=='),
-        ('ADD', r'\+'),
-        ('SUB', r'-'),
-        ('MUL', r'\*'),
-        ('DIV', r'/'),
-        ('ID', r'[A-Za-z]+'),
-        ('NEWLINE', r'\n'),
-        ('SKIP', r'[ \t\r]+'),
+        ('NUMBER', r'\b\d+\b'),  # Integer numbers
+        ('SET', r'\bSet\b'),  # Set keyword
+        ('TO', r'\bto\b'),  # to keyword
+        ('PRINT', r'\bPrint\b'),  # Print keyword
+        ('IF', r'\bIf\b'),  # If keyword
+        ('THEN', r'\bThen\b'),  # Then keyword
+        ('ELSE', r'\bElse\b'),  # Else keyword
+        ('ENDIF', r'\bEnd If\b'),  # End If keyword
+        ('GT', r'>'),  # Greater than operator
+        ('LT', r'<'),  # Less than operator
+        ('EQ', r'=='),  # Equality operator
+        ('ADD', r'\+'),  # Addition operator
+        ('SUB', r'-'),  # Subtraction operator
+        ('MUL', r'\*'),  # Multiplication operator
+        ('DIV', r'/'),  # Division operator
+        ('ID', r'\b[A-Za-z]+\b'),  # Variable names
+        ('NEWLINE', r'\n'),  # Line endings
+        ('SKIP', r'[ \t\r]+'),  # Skip whitespace and tabs
     ]
+
     token_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
-    tokens = [
-        (match.lastgroup, match.group(match.lastgroup))
-        for match in re.finditer(token_regex, code)
-        if match.lastgroup not in {'SKIP', 'NEWLINE'}
-    ]
+    tokens = []
+
+    for match in re.finditer(token_regex, code):
+        token_type = match.lastgroup
+        token_value = match.group(token_type)
+
+        if token_type == 'NEWLINE':
+            continue  # Skip newline in token list, may handle differently if needed
+
+        elif token_type != 'SKIP':
+            tokens.append((token_type, token_value))
+
     return tokens
